a500813ec6cd75595260e094d68813fa42600859
Fabio Borges <fabioborges.ti@gmail.com>
2022-03-28 10:24:05 -0300

    initial commit

---
+++ tests/Bongo.Models.Tests/DateInFutureAttributeTests.cs
@@ -0,0 +1,27 @@
+﻿using Bongo.Models.ModelValidations;
+using NUnit.Framework;
+using System;
+
+namespace Bongo.Models.Tests
+{
+    [TestFixture]
+    public class DateInFutureAttributeTests
+    {
+        [TestCase(-100, ExpectedResult = false)]
+        [TestCase(100, ExpectedResult = true)]
+        public bool DateValidator_InputExpectedDateRange_DateValidity(int addTime)
+        {
+            DateInFutureAttribute dateInFutureAttribute = new(() => DateTime.Now);
+
+            return dateInFutureAttribute.IsValid(DateTime.Now.AddSeconds(addTime));
+        }
+
+        [Test]
+        public void DateValidator_NotValidDate_ReturnErrorMessage()
+        {
+            var result = new DateInFutureAttribute();
+
+            Assert.AreEqual("Date must be in the future", result.ErrorMessage);
+        }
+    }
+}
---
+++ tests/Bongo.Web.Tests/RoomBookingControllerTests.cs
@@ -0,0 +1,62 @@
+﻿using Bongo.Core.Services.IServices;
+using Bongo.Models.Model;
+using Bongo.Models.Model.VM;
+using Bongo.Web.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+
+namespace Bongo.Web.Tests
+{
+    [TestFixture]
+    public class RoomBookingControllerTests
+    {
+        private readonly Mock<IStudyRoomBookingService> _studyRoomBookingService = new();
+
+        private RoomBookingController _bookingController;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _bookingController = new RoomBookingController(_studyRoomBookingService.Object);
+        }
+
+        [Test]
+        public void IndexPage_CallRequest_VerifyGetAllInvoked()
+        {
+            _bookingController.Index();
+
+            _studyRoomBookingService
+                .Verify(x => x.GetAllBooking(), Times.Once());
+        }
+
+        [Test]
+        public void BookRoomCheck_ModelStateInvalid_ReturnView()
+        {
+            _bookingController.ModelState.AddModelError("test", "test");
+
+            var result = _bookingController.Book(new StudyRoomBooking());
+
+            var viewResult = result as ViewResult;
+
+            Assert.AreEqual("Book", viewResult?.ViewName);
+        }
+
+        [Test]
+        public void BookRoomCheck_NotSuccessful_NoRoomCode()
+        {
+            _studyRoomBookingService
+                .Setup(x => x.BookStudyRoom(It.IsAny<StudyRoomBooking>()))
+                .Returns(new StudyRoomBookingResult
+                {
+                    Code = StudyRoomBookingCode.NoRoomAvailable,
+                });
+
+            var result = _bookingController.Book(new StudyRoomBooking());
+
+            var viewResult = result as ViewResult;
+
+            Assert.AreEqual("No Study Room available for selected date", viewResult?.ViewData["Error"]);
+        }
+    }
+}
